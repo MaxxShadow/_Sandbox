424a24b63ffb5c9502aaf254224dea6c88049c44
MaxxShadow <99481513+MaxxShadow@users.noreply.github.com>
2025-05-22 10:04:08 +0200

    Create testsnip1.py

---
+++ testsnip1.py
@@ -0,0 +1,12 @@
+#--- Testovací Python snippet
+#print("Hello, World!")
+
+def test():
+    print("--- Hola world ---")
+    print
+    print("--konec--")
+
+if __name__ == "__main__":
+    test()
+
+#-- end of code ---
